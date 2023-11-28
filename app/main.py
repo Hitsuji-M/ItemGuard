f856bbd66e3c238281cf9ee1b99e3da3d82ad02e
ClementTh <clement.thiveyrat@edu.esiee.fr>
2023-11-28 15:00:06 +0100

    avec front

--- app/main.py
+++ app/main.py
@@ -41,6 +41,7 @@ def final_auth(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = D
 def register_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     model = UserModel(email=form_data.username, passwd=form_data.password)
     auth_services.register(db, model)
+    print(auth_services.login(db, model))
     return auth_services.login(db, model)
 
 @app.post("/user/login")
--- front/app.py
+++ front/app.py
@@ -3,9 +3,58 @@ import requests
 
 api_url = "http://api:5000"
 
+
+
+
+def all_product(token):
+    if st.button("Récupérer les données depuis FastAPI"):
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(f"{api_url}/products", headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            st.write("Produits: ")
+            st.write(data)
+        else:
+            st.error("Échec de la récupération des données depuis FastAPI.")
+
+
+def create(token):
+    st.header("Create")
+
+    with st.form("create_product_form"):
+        product_name = st.text_input("Nom du produit", key="product_name")
+        product_price = st.number_input("Prix du produit", key="product_price")
+
+        submit_button = st.form_submit_button("Submit")
+
+    if submit_button:
+        product_data = {"name": product_name, "price": product_price}
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.post(f"{api_url}/product", headers=headers, json=product_data)
+
+        if response.status_code == 200:
+            st.success("Produit créé avec succès!")
+            all_product(token)
+        else:
+            st.error("Échec de la création du produit.")
+
+
+
+
+
+def navigation(token):
+    if st.session_state.is_authenticated:
+        all_product(token)
+        create(token)
+
+
+        
+
+
 def main():
-    st.title("Application Streamlit avec Authentification")
+    st.title("ItemGuard")
     st.session_state.setdefault('is_authenticated', False)
+    token = None
 
     # Page de connexion
     if not st.session_state.is_authenticated:
@@ -18,30 +67,18 @@ def main():
             # Faire la requête d'authentification à votre API FastAPI
             response = requests.post(
                 f"{api_url}/auth",
-                data={"grant_type":"password","username": username, "password": password}
+                data={"grant_type": "password", "username": username, "password": password}
             )
 
             if response.status_code == 200:
                 st.session_state.is_authenticated = True
+                token = response.json().get("access_token")
                 st.success("Connexion réussie! Vous pouvez accéder au reste de l'application.")
-            else:
-                print(response.reason)
-                st.error("Échec de la connexion. Veuillez vérifier vos informations d'identification.")
-
-    
-    
-            st.header("Bienvenue dans l'application")
-
-            if st.button("Récupérer les données depuis FastAPI"):
-                # Faire la requête pour récupérer les données
-                response = requests.get(f"{api_url}/log/last")
+                st.write(token)
                 
-                if response.status_code == 200:
-                    data = response.json()
-                    st.write("Name: ")
-                    st.write(data)
-                else:
-                    st.error("Échec de la récupération des données depuis FastAPI.")
-
+    if st.session_state.is_authenticated:
+        navigation(token)
+    
+        
 if __name__ == '__main__':
     main()
